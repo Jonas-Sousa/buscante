8675cdf63ba269d907afcd1d707585cc50b789c8
Jonas sousa <jonassousagtr@gmail.com>
2023-06-03 16:17:41 -0300

    Feat: implementando a busca dinâmica no site.

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -16,8 +16,7 @@ export class LivroService {
   buscar(valorDigitado: string): Observable<Item[]> {
     const params = new HttpParams().append('q', valorDigitado )
     return this.http.get<LivrosResultado>(this.API, { params }).pipe(
-      tap(retornoAPI => console.log('Fluxo do tap', retornoAPI)),
-      map(resultado => resultado.items),
+      map(resultado => resultado.items)
     )
   }
 
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,34 +1,34 @@
 import { LivroVolumeInfo } from './../../models/livroVolumeInfo';
 import { LivroService } from './../../service/livro.service';
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Livro } from 'src/app/models/Livro.interface';
+import { Component } from '@angular/core';
+import { switchMap, map, tap, filter, debounceTime } from 'rxjs';
 import { Item } from 'src/app/models/Item.interface';
+import { FormControl } from '@angular/forms';
+
+const PAUSA = 400
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy{
+export class ListaLivrosComponent {
 
-  listaLivros: Livro[]
-  campoBusca: string = ''
-  subscription: Subscription
-  livro: Livro
+
+  campoBusca = new FormControl()
 
   constructor(private service: LivroService) { }
 
-  buscarLivros(){
-    this.subscription = this.service.buscar(this.campoBusca).subscribe({
-      next: (items) => {
-        this.listaLivros = this.livrosResultadoParaLivros(items)
-      },
-      error: erro => console.log(erro)
-
-    })
-    this.campoBusca = ''
-  }
+  livrosEncontrados$ = this.campoBusca.valueChanges
+  .pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 4),
+    tap(() => console.log('Fluxo')),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    tap((api) => console.log(api)),
+    map((items) => this.livrosResultadoParaLivros(items)
+    )
+  )
 
   livrosResultadoParaLivros(itens: Item[]): LivroVolumeInfo[]{
       return itens.map(item => {
@@ -36,10 +36,6 @@ export class ListaLivrosComponent implements OnDestroy{
     })
   }
 
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe()
-  }
 }
 
 
